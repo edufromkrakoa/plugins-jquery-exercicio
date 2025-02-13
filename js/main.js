fd90b51bd1ab3e24608758271160a0199b446f55
edufromkrakoa <wikialmeida@gmail.com>
2025-02-12 22:51:01 -0300

    Correção do jQuery

--- js/main.js
+++ js/main.js
@@ -8,15 +8,15 @@ $(function() {
 
 $('#telefone').mask('(00) 00000-0000', {
     placeholder: '(DDD) 12345-6789'
-})
+});
 
 $('#cpf').mask('000.000.000-00', {
     placeholder: '123.456.789-00'
-})
+});
 
 $('#cep').mask('00000-000', {
     placeholder: '012345-678'
-})
+});
 
 $('form').validate({
     rules: {
@@ -39,20 +39,20 @@ $('form').validate({
         cpf: {
             required: true
         }
-        },
-        messages: {
-            nome: "Por favor, insira seu nome completo.",
-            email: "Por favor, insira um endereço de email válido.",
-            telefone: "Por favor, insira seu telefone.",
-            endereco: "Por favor, insira seu endereço completo.",
-            cep: "Por favor, insira seu CEP.",
-            cpf: "Por favor, insira seu CPF."
-        },
-        submitHandler: function (form) {
-            alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-            form.reset();
-        },
-        invalidHandler: function (form, validator) {
-            alert("Por favor, preencha os campos para prosseguir com a compra!");
-        }
-    });
+    },
+    messages: {
+        nome: "Por favor, insira seu nome completo.",
+        email: "Por favor, insira um endereço de email válido.",
+        telefone: "Por favor, insira seu telefone.",
+        endereco: "Por favor, insira seu endereço completo.",
+        cep: "Por favor, insira seu CEP.",
+        cpf: "Por favor, insira seu CPF."
+    },
+    submitHandler: function (form) {
+        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+        form.reset();
+    },
+    invalidHandler: function (form, validator) {
+        alert("Por favor, preencha os campos para prosseguir com a compra!");
+    }
+});
